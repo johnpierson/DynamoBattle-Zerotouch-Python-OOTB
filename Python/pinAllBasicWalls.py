68adc6b93ea483c39a9f0093d8b3bb13ee471080
john pierson <sixtysecondrevit@gmail.com>
2022-03-16 14:21:30 -0600

    Upload all Files

--- Python/pinAllBasicWalls.py
+++ Python/pinAllBasicWalls.py
@@ -12,14 +12,16 @@ doc = DocumentManager.Instance.CurrentDBDocument
 
 basicWalls = []
 
-allWalls = FilteredElementCollector(doc).OfClass(Wall).ToElements()
+allWalls = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Walls).WhereElementIsNotElementType().ToElements()
 
 TransactionManager.Instance.EnsureInTransaction(doc)
+
 for wall in allWalls:
+
 	if wall.WallType.Kind == WallKind.Basic:
 		wall.Pinned = True
 		basicWalls.append(wall)
+		
 TransactionManager.Instance.TransactionTaskDone()
 
-# Assign your output to the OUT variable.
 OUT = basicWalls
---
+++ ZeroTouch/_src/PinAllBasicWalls/src/HelloDynamo.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Autodesk.Revit.DB;
+using Revit.Elements;
+using RevitServices.Persistence;
+using RevitServices.Transactions;
+using Wall = Revit.Elements.Wall;
+
+namespace PinAllBasicWalls
+{
+    /// <summary>
+    /// Hello World basic sample
+    /// </summary>
+    public static class HelloDynamo
+    {
+        /// <summary>
+        /// Prints a friendly greeting
+        /// </summary>
+        /// <returns>A greeting</returns>
+        public static List<Revit.Elements.Wall> PinAllBasicWalls()
+        {
+            var doc = DocumentManager.Instance.CurrentDBDocument;
+
+            List<Revit.Elements.Wall> basicWalls = new List<Wall>();
+
+            var allWalls = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Walls)
+                .WhereElementIsNotElementType().ToElements();
+
+            TransactionManager.Instance.EnsureInTransaction(doc);
+
+            foreach (Autodesk.Revit.DB.Wall wall in allWalls)
+            {
+                if (wall.WallType.Kind == WallKind.Basic)
+                {
+                    wall.Pinned = true;
+
+                    Revit.Elements.Wall dynamoWall = (Revit.Elements.Wall)wall.ToDSType(true);
+
+                    basicWalls.Add(dynamoWall); 
+                }
+                
+            }
+
+            TransactionManager.Instance.TransactionTaskDone();
+
+            return basicWalls;
+        }
+    }
+}
